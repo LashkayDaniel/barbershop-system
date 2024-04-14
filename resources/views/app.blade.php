5e78ee38178105d15c103f91e723fd899704597a
lashkaydaniel <lashkay74y@gmail.com>
2024-04-14 14:01:48 +0300

    some fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 </head>
-<body class="font-sans antialiased scroll-smooth {{ request()->is('/') ? 'bg-body' : 'bg-white' }}">
+<body class="font-sans antialiased scroll-smooth bg-body">
 @inertia
 </body>
 </html>

e40bfeb5c6623d748136fb1681768ce72b0ca393
lashkaydaniel <lashkay74y@gmail.com>
2024-04-21 13:02:16 +0300

    adding discount relation

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Service extends Model
 {
     use HasFactory;
 
     public $fillable = ['name', 'description'];
-
 
     public function users(): BelongsToMany
     {
@@ -23,5 +24,13 @@ class Service extends Model
     public function orders(): HasMany
     {
         return $this->hasMany(Order::class);
+    }
+
+    public function discount(): HasOne
+    {
+        $todayDate = Carbon::today()->toDateString();
+        return $this->hasOne(Discount::class)
+            ->whereDate('start', '<=', $todayDate)
+            ->whereDate('end', '>=', $todayDate);
     }
 }

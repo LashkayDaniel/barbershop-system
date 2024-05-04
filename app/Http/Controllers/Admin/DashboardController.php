764844c6613a676450d5a131a75303203677b00f
lashkaydaniel <lashkay74y@gmail.com>
2024-05-04 22:22:47 +0300

    creating admin dashboard with charts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,35 +3,148 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use App\Models\Order;
+use App\Models\Response;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $income = Order::query()
+        $incomeByYearsMonths = Order::query()
             ->selectRaw(
-                'YEAR(orders.created_at) as year, MONTHNAME(orders.created_at) as month, COUNT(*) as orders, SUM(service_user.price) as income'
+                'YEAR(orders.created_at) as year, MONTHNAME(orders.created_at) as month, COUNT(*) as orders, SUM(service_user.price * (1-IFNULL(discounts.percent,0)/100)) as income'
             )
             ->join('service_user', function ($join) {
                 $join->on('orders.user_id', '=', 'service_user.user_id')
                     ->on('orders.service_id', '=', 'service_user.service_id');
             })
+            ->leftJoin('discounts', 'discounts.id', '=', 'orders.discount_id')
             ->groupBy('month', 'year')
             ->get();
 
-        $totalIncome = $income->sum('income');
+        $totalIncome = number_format(round($incomeByYearsMonths->sum('income')));
+
+        $currentYear = Carbon::now()->year;
+        $currentMonth = Carbon::now()->month;
+        $incomeByPrevMonth = $this->calcIncomeByMonth($currentYear, $currentMonth - 1);
+        $incomeByCurrentMonth = $this->calcIncomeByMonth($currentYear, $currentMonth);
+        $incomePercentDiff = $this->calcPercentDiff($incomeByCurrentMonth, $incomeByPrevMonth);
+
+        $totalOrders = Order::all()->count();
+        $ordersByPrevMonth = $this->calcCountByMonth(Order::class, $currentYear, $currentMonth - 1);
+        $ordersByCurrentMonth = $this->calcCountByMonth(Order::class, $currentYear, $currentMonth);
+        $ordersPercentDiff = $this->calcPercentDiff($ordersByCurrentMonth, $ordersByPrevMonth);
+
+        $totalCustomers = Client::all()->count();
+        $customersByPrevMonth = $this->calcCountByMonth(Client::class, $currentYear, $currentMonth - 1);
+        $customersByCurrentMonth = $this->calcCountByMonth(Client::class, $currentYear, $currentMonth);
+        $customersPercentDiff = $this->calcPercentDiff($customersByCurrentMonth, $customersByPrevMonth);
+
+        $positiveReviews = Response::query()->where('rating', 5)->count();
+        $reviewsByPrevMonth = $this->calcReviewsByMonth($currentYear, $currentMonth - 1);
+        $reviewsByCurrentMonth = $this->calcReviewsByMonth($currentYear, $currentMonth);
+        $reviewsPercentDiff = $this->calcPercentDiff($reviewsByCurrentMonth, $reviewsByPrevMonth);
+
+        $popularServicesData = Order::with('service')
+            ->select('service_id', DB::raw('count(*) as total_orders'))
+            ->groupBy('service_id')
+            ->get();
+
+        $popularServices = $popularServicesData->map(function ($item) use ($totalOrders) {
+            return [
+                'name' => $item->service->name,
+                'percent' => round(($item->total_orders / $totalOrders) * 100, 1),
+            ];
+        });
+
+        $employeesStatistic = Order::query()
+            ->selectRaw(
+                'users.name as employee, COUNT(*) as orders, SUM(service_user.price * (1-IFNULL(discounts.percent,0)/100)) as income'
+            )
+            ->join('service_user', function ($join) {
+                $join->on('orders.user_id', '=', 'service_user.user_id')
+                    ->on('orders.service_id', '=', 'service_user.service_id');
+            })
+            ->join('users', 'users.id', '=', 'orders.user_id')
+            ->leftJoin('discounts', 'discounts.id', '=', 'orders.discount_id')
+            ->groupBy('employee')
+            ->orderByDesc('income')
+            ->get();
 
         return Inertia::render(
             'Admin/Dashboard/Index',
             [
                 'statistics' => [
-                    'totalIncome' => $totalIncome,
-                    'income' => $income,
+                    'income' => [
+                        'total' => $totalIncome,
+                        'income' => $incomeByYearsMonths,
+                        'last_month' => $incomePercentDiff,
+                    ],
+                    'reservation' => [
+                        'total' => number_format($totalOrders),
+                        'last_week' => $ordersPercentDiff,
+                    ],
+                    'customers' => [
+                        'total' => number_format($totalCustomers),
+                        'last_week' => $customersPercentDiff,
+                    ],
+                    'reviews' => [
+                        'total' => number_format($positiveReviews),
+                        'last_week' => $reviewsPercentDiff,
+                    ],
+                    'popular_services' => $popularServices,
+                    'employees_data' => $employeesStatistic,
                 ],
             ]
         );
     }
 
+    private function calcIncomeByMonth(int $year, int $month): int
+    {
+        return round(
+            Order::query()
+                ->whereYear('created_at', $year)
+                ->whereMonth('created_at', $month)
+                ->get()
+                ->map(function ($order) {
+                    $discount = $order?->discount;
+                    $price = DB::table('service_user')->where('user_id', $order->user_id)
+                        ->where('service_id', $order->service_id)->value('price');
+                    $order->price = $price;
+                    if ($discount) {
+                        $order->price = $price * (1 - $discount->percent / 100);
+                    }
+                    return $order;
+                })
+                ->sum('price')
+        );
+    }
+
+    private function calcPercentDiff(float $num1, float $num2): float
+    {
+        $incomeDiff = $num1 - $num2;
+        return round(($incomeDiff / max($num1, $num2)) * 100, 2);
+    }
+
+    private function calcCountByMonth($model, int $year, int $month): int
+    {
+        return $model::query()
+            ->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->count();
+    }
+
+    private function calcReviewsByMonth(int $year, int $month): int
+    {
+        return Response::query()
+            ->where('rating', 5)
+            ->whereYear('created_at', $year)
+            ->whereMonth('created_at', $month)
+            ->count();
+    }
 }
+
--- resources/js/Pages/Admin/Dashboard/Index.vue
+++ resources/js/Pages/Admin/Dashboard/Index.vue
@@ -21,56 +21,20 @@
                     </svg>
                 </div>
                 <div class="p-4 text-right">
-                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">Today's
-                        Money</p>
+                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">
+                        Загальний дохід
+                    </p>
                     <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
-                        $53k</h4>
+                        {{ statistics?.income?.total }}₴
+                    </h4>
                 </div>
                 <div class="border-blue-gray-50 border-t p-4">
-                    <p class="text-blue-gray-600 block font-sans text-base font-normal leading-relaxed antialiased">
-                        <strong class="text-green-500">+55%</strong>&nbsp;than last week</p>
-                </div>
-            </div>
-            <div
-                class="border-blue-gray-100 relative flex flex-col rounded-xl border bg-white bg-clip-border text-gray-700 shadow-sm">
-                <div
-                    class="absolute mx-4 mt-4 grid h-12 w-12 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-gray-900 to-gray-800 bg-clip-border text-white shadow-gray-900/20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
-                         class="h-6 w-6 text-white">
-                        <path
-                            d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z"></path>
-                    </svg>
-                </div>
-                <div class="p-4 text-right">
-                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">Today's
-                        Users</p>
-                    <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
-                        2,300</h4>
-                </div>
-                <div class="border-blue-gray-50 border-t p-4">
-                    <p class="text-blue-gray-600 block font-sans text-base font-normal leading-relaxed antialiased">
-                        <strong class="text-green-500">+3%</strong>&nbsp;than last month</p>
-                </div>
-            </div>
-            <div
-                class="border-blue-gray-100 relative flex flex-col rounded-xl border bg-white bg-clip-border text-gray-700 shadow-sm">
-                <div
-                    class="absolute mx-4 mt-4 grid h-12 w-12 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-gray-900 to-gray-800 bg-clip-border text-white shadow-gray-900/20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
-                         class="h-6 w-6 text-white">
-                        <path
-                            d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                    </svg>
-                </div>
-                <div class="p-4 text-right">
-                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">New
-                        Clients</p>
-                    <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
-                        3,462</h4>
-                </div>
-                <div class="border-blue-gray-50 border-t p-4">
-                    <p class="text-blue-gray-600 block font-sans text-base font-normal leading-relaxed antialiased">
-                        <strong class="text-red-500">-2%</strong>&nbsp;than yesterday</p>
+                    <p class="text-blue-gray-600 block font-sans leading-relaxed antialiased">
+                        <strong class="text-green-500"
+                                :class="{'text-red-500':percentFormat(statistics?.income?.last_month).slice(0,1).toString() === '-'}">
+                            {{ percentFormat(statistics?.income?.last_month) }}
+                            <span class="text-sm">%</span>
+                        </strong> ніж за минулий місяць</p>
                 </div>
             </div>
             <div
@@ -85,33 +49,106 @@
                 </div>
                 <div class="p-4 text-right">
                     <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">
-                        Sales</p>
+                        Бронювань
+                    </p>
                     <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
-                        $103,430</h4>
+                        {{ statistics?.reservation.total }}
+                    </h4>
                 </div>
                 <div class="border-blue-gray-50 border-t p-4">
-                    <p class="text-blue-gray-600 block font-sans text-base font-normal leading-relaxed antialiased">
-                        <strong class="text-green-500">+5%</strong>&nbsp;than yesterday</p>
+                    <p class="text-blue-gray-600 block font-sans leading-relaxed antialiased">
+                        <strong class="text-green-500"
+                                :class="{'text-red-500':percentFormat(statistics?.reservation?.last_week).slice(0,1).toString() === '-'}">
+                            {{ percentFormat(statistics?.reservation?.last_week) }}
+                            <span class="text-sm">%</span>
+                        </strong> ніж за минулий місяць</p>
+                </div>
+            </div>
+            <div
+                class="border-blue-gray-100 relative flex flex-col rounded-xl border bg-white bg-clip-border text-gray-700 shadow-sm">
+                <div
+                    class="absolute mx-4 mt-4 grid h-12 w-12 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-gray-900 to-gray-800 bg-clip-border text-white shadow-gray-900/20">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+                         class="h-6 w-6 text-white">
+                        <path
+                            d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                    </svg>
+                </div>
+                <div class="p-4 text-right">
+                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">
+                        Клієнтів
+                    </p>
+                    <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                        {{ statistics?.customers.total }}
+                    </h4>
+                </div>
+                <div class="border-blue-gray-50 border-t p-4">
+                    <p class="text-blue-gray-600 block font-sans leading-relaxed antialiased">
+                        <strong class="text-green-500"
+                                :class="{'text-red-500':percentFormat(statistics?.customers?.last_week).slice(0,1).toString() === '-'}">
+                            {{ percentFormat(statistics?.customers?.last_week) }}
+                            <span class="text-sm">%</span>
+                        </strong> ніж за минулий місяць</p>
+                </div>
+            </div>
+            <div
+                class="border-blue-gray-100 relative flex flex-col rounded-xl border bg-white bg-clip-border text-gray-700 shadow-sm">
+                <div
+                    class="absolute mx-4 mt-4 grid h-12 w-12 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-gray-900 to-gray-800 bg-clip-border text-white shadow-gray-900/20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         aria-hidden="true" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605"/>
+                    </svg>
+                </div>
+                <div class="p-4 text-right">
+                    <p class="text-blue-gray-600 block font-sans text-sm font-normal leading-normal antialiased">
+                        Позитивних відгуків
+                    </p>
+                    <h4 class="text-blue-gray-900 block font-sans text-2xl font-semibold leading-snug tracking-normal antialiased">
+                        {{ statistics?.reviews.total }}
+                    </h4>
+                </div>
+                <div class="border-blue-gray-50 border-t p-4">
+                    <p class="text-blue-gray-600 block font-sans leading-relaxed antialiased">
+                        <strong class="text-green-500"
+                                :class="{'text-red-500':percentFormat(statistics?.reviews?.last_week).slice(0,1).toString() === '-'}">
+                            {{ percentFormat(statistics?.reviews?.last_week) }}
+                            <span class="text-sm">%</span>
+                        </strong> ніж за минулий місяць</p>
                 </div>
             </div>
         </div>
 
-        <div class="bg-slate-200 p-6 flex items-center">
-
-            <div class="flex-1 max-w-xl border rounded-lg border-gray-secondary">
-                <div class="flex justify-between items-center p-2  mb-2 border-b border-gray-secondary">
-                    <h2 class="font-bold tracking-wide text-xl text-gray-light">Дохід</h2>
+        <div class="bg-slate-200 p-6 flex items-stretch">
+            <div class="flex-1 border rounded-lg border-gray-secondary">
+                <div class="flex justify-between items-center p-2 mb-2 border-b border-gray-secondary">
+                    <h2 class="font-bold tracking-wide text-xl text-gray-light">Доходи</h2>
                     <select v-model="incomeConfig.year"
                             class="bg-transparent font-bold text-gold-secondary border-0 border-b-2 border-gold-primary focus:ring-gold-primary focus:rounded-lg cursor-pointer">
                         <option v-for="(key,year) in groupedIncomesByYears"
-                                :key="year"
+                                :key="key"
                                 :value="year">{{ year }}
                         </option>
                     </select>
                 </div>
-                <apexchart ref="incomeChart" type="area" height="160" :options="incomeChartOptions"
+                <apexchart ref="incomeChart" type="area" :options="incomeChartOptions"
                            :series="incomeChartSeries"></apexchart>
             </div>
+            <div class="p-4 ml-4 flex-1 border rounded-lg border-gray-secondary">
+                <h2 class="font-bold tracking-wide text-xl text-gray-light">Популярні послуги</h2>
+                <apexchart type="pie"
+                           :options="popularServicesOptions"
+                           :series="popularServicesSeries"
+                />
+            </div>
+        </div>
+        <div class="bg-slate-200 p-6 mt-2">
+            <apexchart type="bar"
+                       height="450"
+                       :options="employeesDataOptions"
+                       :series="employeesDataSeries"
+            />
         </div>
 
     </AuthenticatedLayout>
@@ -119,6 +156,7 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {currencyFormat} from '@/utils.js'
 
 import {Head} from '@inertiajs/vue3';
 import {computed, onMounted, reactive, ref, watch} from "vue";
@@ -141,23 +179,28 @@ const incomeConfig = reactive({
 
 
 const groupedIncomesByYears = computed(() => {
-    return Object.groupBy(props.statistics?.income, i => i.year)
+    return Object.groupBy(props.statistics?.income.income, i => i.year)
 })
+
+const percentFormat = (num) => {
+    return num < 0 ? num + '' : `+${num}`;
+}
+
 
 onMounted(() => {
     const lastYear = Object.keys(groupedIncomesByYears.value).pop()
     incomeConfig.year = lastYear
-    changeYearHandler(lastYear)
+    changeIncomeYearHandler(lastYear)
 })
 
 watch(
     () => incomeConfig.year,
     () => {
-        changeYearHandler(incomeConfig.year)
+        changeIncomeYearHandler(incomeConfig.year)
     }
 )
 
-function changeYearHandler(year) {
+function changeIncomeYearHandler(year) {
     const profitByYear = groupedIncomesByYears.value[year]
     incomeConfig.categories = Object.values(months)
 
@@ -171,7 +214,6 @@ function changeYearHandler(year) {
         name: 'Дохід',
         data: incomeConfig.categoriesData
     }]
-    console.log(typeof incomeConfig.totalYearSum)
     incomeChartOptions = {
         ...incomeChartOptions, ...{
             xaxis: {
@@ -184,16 +226,10 @@ function changeYearHandler(year) {
     }
 }
 
-
-const currencyFormat = (number) => {
-    return new Intl.NumberFormat('uk-UA', {style: 'currency', currency: 'UAH'}).format(number)
-}
-
 let incomeChartSeries = []
 let incomeChartOptions = {
     chart: {
         type: 'area',
-        height: 160,
         sparkline: {
             enabled: true
         },
@@ -240,6 +276,83 @@ let incomeChartOptions = {
         marker: {
             show: false
         }
+    }
+}
+
+const popularServicesTitle = computed(() => {
+    return props.statistics?.popular_services.map(i => i.name)
+})
+const popularServicesSeries = computed(() => {
+    return props.statistics?.popular_services.map(i => i.percent)
+})
+const popularServicesOptions = {
+    chart: {
+        type: 'pie',
+        width: '100%',
+        height: '100%'
+    },
+
+    colors: ['#00D8B6', '#008FFB', '#FEB019', '#FF4560', '#775DD0'],
+    labels: popularServicesTitle.value,
+    legend: {
+        position: 'left',
+        offsetY: 80
+    },
+    tooltip: {
+        y: {
+            formatter: function (value) {
+                return value + '%';
+            }
+        }
+    }
+}
+
+
+const employeeNames = computed(() => {
+    return props.statistics?.employees_data.map(i => i.employee);
+})
+
+const employeeOrders = computed(() => {
+    return props.statistics?.employees_data.map(i => i.orders);
+})
+
+const employeeIncome = computed(() => {
+    return props.statistics?.employees_data.map(i => i.income);
+})
+
+const employeesDataSeries = [
+    {name: 'Загальний прибуток', data: employeeIncome.value},
+    {name: 'Бронювань', data: employeeOrders.value},
+
+]
+
+const employeesDataOptions = {
+    chart: {
+        type: 'bar',
+        height: 450
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+    },
+    plotOptions: {
+        bar: {
+            borderRadius: 4,
+            horizontal: true,
+        }
+    },
+    title: {
+        text: 'Статистика працівників',
+        style: {
+            fontSize: '18px'
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    xaxis: {
+        categories: employeeNames.value
     }
 }
 
